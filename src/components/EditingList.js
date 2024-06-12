17d3f3de35ee0640adfeda2b4f4f52327c177416
Jelynfish <thepixeltomato@gmail.com>
2024-06-12 16:18:03 -0400

    modal skeleton

--- src/components/EditingList.js
+++ src/components/EditingList.js
@@ -1,22 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const EditingList = ({ list, fields, onAdd, onEdit, onDelete }) => {
-  return (
+    const [openModal, setOpenModal] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+
+    const handleDeleteClick = (item) => {
+        setModalContent(
+            <div>
+                <p>Are you sure you want to delete this item?</p>
+
+                <button onClick={handleCloseModal}>Cancel</button>
+                <button onClick={() => handleConfirmDelete(item)}>Delete</button>
+            </div>
+        )
+        setOpenModal(true);
+    }
+    const handleConfirmDelete = (item) => {
+        if (item) {
+            onDelete(item);
+        }
+        handleCloseModal();
+    }
+
+    const handleAddClick = () => { 
+        setModalContent(
+            <div>
+                <p>Add new item</p>
+                <button onClick={handleCloseModal}>Cancel</button>
+                <button onClick={() => handleConfirmAdd()}>Add</button>
+            </div>
+        )
+        setOpenModal(true);
+    }
+    const handleConfirmAdd = () => {
+        onAdd();
+        handleCloseModal();
+    }
+
+    const handleEditClick = (item) => {
+        setModalContent(
+            <div>
+                <p>Edit item</p>
+                {fields.map((field) => (
+                    <div key={field}>
+                        <label>{field}</label>
+                        <input type="text" name={field} />
+                    </div>
+                ))}
+
+                <button onClick={handleCloseModal}>Cancel</button>
+                <button onClick={() => handleConfirmEdit(item)}>Add</button>
+            </div>
+        )
+        setOpenModal(true);
+    }
+    const handleConfirmEdit = (item) => {
+        onEdit(item);
+        handleCloseModal();
+    }
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+        setModalContent(null);
+    }
+    return (
     <div>
       <h2>Editing List</h2>
-
       <ul>
         {list.map((item) => (
           <li key={item._id}>
             {fields.map((field) => (
               <span key={field}>{item[field]} </span>
             ))}
-            <button onClick={() => onEdit(item)}>Edit</button>
-            <button onClick={() => onDelete(item)}>Delete</button>
+            <button onClick={() => handleEditClick(item)}>Edit</button>
+            <button onClick={() => handleDeleteClick(item)}>Delete</button>
           </li>
         ))}
       </ul>
-      <button onClick={onAdd}>Add</button>
+      <button onClick={handleAddClick}>Add</button>
+
+      <Modal isOpen={openModal} children={modalContent} />
     </div>
   );
 };
